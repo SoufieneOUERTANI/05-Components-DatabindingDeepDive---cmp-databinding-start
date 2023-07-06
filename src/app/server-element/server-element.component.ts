2b7f31a3845eefb45a4c9d85d79d3dcb4228ffc3
Soufiene OUERTANI <soufiene.ouertani@gmail.com>
2023-07-06 19:38:30 +0200

    81. Access to ng-content with @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, ContentChild, ElementRef, Input, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -10,6 +10,10 @@ export class ServerElementComponent {
   @Input('srvElement') element: { type: string, name: string, content: string }
   @Input() name  : string;
   @ViewChild('heading', {static:true}) header : ElementRef ;
+  // @ContentChild : Accès à un propiété d'un autre composant, 
+  // Transmission rendue possible par l'utilisation de la référence "ng-content" de ce composant "server-element"
+  //  dans le composant contenant la référence "app"
+  @ContentChild('contentParagraph', {static:true}) paragraph : ElementRef ;
 
   ServerElementComponent(){
     console.log('constructor called');
@@ -23,6 +27,8 @@ export class ServerElementComponent {
   ngOnInit(){
     console.log('ngOnInit called');
     console.log(">>> : "+this.header.nativeElement.textContent);
+    console.log(">>> : "+this.paragraph.nativeElement.textContent);
+
   }
   
   ngDoCheck(){
@@ -31,6 +37,7 @@ export class ServerElementComponent {
   
   ngAfterContentInit(){
     console.log('ngAfterContentInit called');
+    console.log(">>> : "+this.paragraph.nativeElement.textContent);
   }
   
   ngAfterContentChecked(){
